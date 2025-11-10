ccb11374337a2cb7abb1ec24fd2c71d913cf9243
David <davidterreu@hotmail.com>
2025-11-10 09:43:37 +0100

    Añadido títulos ejercicios

--- DWES_David_Terreu_Ejercicios_finales_Tema_2.php
+++ DWES_David_Terreu_Ejercicios_finales_Tema_2.php
@@ -1,1 +1,20 @@
 <?php
+
+//Ejercicio 1. Calculadora
+echo "Ejercicio 1";
+echo "\n";
+
+//Ejercicio 2. Validador de Formularios
+echo "\n\n";
+echo "Ejercicio 2";
+echo "\n";
+
+//Ejercicio 3. Manipulación de Arrays
+echo "\n\n";
+echo "Ejercicio 3";
+echo "\n";
+
+//Ejercicio 4. Procesador de Texto
+echo "\n\n";
+echo "Ejercicio 4";
+echo "\n";
